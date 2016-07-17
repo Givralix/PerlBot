cb17580df4046946a39c6fa77787867f90c619f9
Givralix <amelie.lulia68@gmail.com>
2016-07-17 12:25:35 +0200

    made it so the authentification parameters aren't directly in the code anymore (they're in a file that isn't in the repository)

--- test1.pl
+++ test1.pl
@@ -13,60 +13,48 @@ sub posting_text {
 	my($blog) = $_[0];
 	my($body) = $_[1];
 	utf8::decode($body);
-	my(%oauth_api_params) = $_[2];
 	my $url = 'http://api.tumblr.com/v2/blog/' . $blog . '/post';
+	open my $f, "<", "tokens"
+		or die "Couldn't open token file: $!";
+	my $file = <$f>;
+	my @tokens = split(';', $file);
+	close $f;
 	my $request =
 		Net::OAuth->request("protected resource")->new
-			(request_ul => $url,
-			%oauth_api_params,
+			(request_url => $url,
+			'consumer_key' => $tokens[0],
+			'consumer_secret' => $tokens[1],
+			'token' => $tokens[2],
+			'token_secret' => $tokens[3],
+			'signature_method' => 'HMAC-SHA1',
+			request_method => 'POST',
 			timestamp => time(),
 			nonce => rand(1000000),
 			extra_params => {
 				'type' => 'text',
 				'body' => $body,
-		});
+		},);
 	$request->sign;
 	my $ua = LWP::UserAgent->new;
 	my $response = $ua->request(POST $url, Content => $request->to_post_body);
+	my $date = localtime();
 	if ( $response->is_success ) {
 		my $r = decode_json($response->content);
 		if($r->{'meta'}{'status'} == 201) {
 			my $item_id = $r->{'reponse'}{'id'};
-			my $date = localtime();
 			print "[$date] Following tumblr entry added:\n$body";
 		} else {
-			my $date = localtime();
 			printf("[$date] Cannot create Tumblr entry: %s\n",
 				$r->{'meta'}{'msg'});
 		}
 	} else {
-	my $date = localtime();
 	printf("[$date] Cannot create Tumblr entry: %s\n",
 			$response->as_string);
 	}
 	return;
 }
 
-my $mc = StupidMarkov->new();
-my @files = ['shitpost_database'];
-$mc->add_files(@files);
-my $mc = String::Markov->new(order => 1, sep => ' ');
-my $body = $mc->generate_sample;
-
 my $blog = 'perlbot.tumblr.com';
-
+my $body = 'test';
 # authentifying
-my %oauth_api_params =
-	('consumer_key' =>
-		'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
-	 'consumer_secret' =>
-		'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
-	 'token' =>
-		'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
-	 'token_secret' =>
-		'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
-	 'signature_method' =>
-		'HMAC-SHA1',
-	 request_method => 'POST',
-);
-posting_text($blog, $body, %oauth_api_params);
+posting_text($blog, $body);
