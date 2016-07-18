e2f755be66ae8c884a3756b1ceea491ec483737a
Givralix <amelie.lulia68@gmail.com>
2016-07-18 23:16:54 +0200

    uses markovify.NewlineText() instead of markovify.Text() and only generates 5 sentences now

--- markov_chain.py
+++ markov_chain.py
@@ -3,10 +3,7 @@ import markovify
 with open("shitpost_database",'r',) as f:
 	text = f.read()
 
-text_model = markovify.Text(text)
+text_model = markovify.NewlineText(text)
 
 for i in range(5):
 	print(text_model.make_sentence())
-
-for i in range(3):
-	print(text_model.make_short_sentence(140))
