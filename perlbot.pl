b9c0d41845e3d2577fbbfed174a6cce477365090
Givralix <amelie.lulia68@gmail.com>
2016-07-22 17:09:37 +0200

    tumblr is shit

--- perlbot.pl
+++ perlbot.pl
@@ -97,36 +97,19 @@ sub getting_answers {
 	my $request_params = %{ $_[1] };
 	my $url = 'http://api.tumblr.com/v2/blog/' . $blog . '/posts/submission';
 	my $request =
-		Net::OAuth->request("protected resource")->new
-			(request_url => $url,
+		Net::OAuth->request("protected resource")->new(
+			request_url => $url,
 			%request_params,
 			request_method => 'GET',
 			timestamp => time(),
 			nonce => rand(1000000),
-		,);
+		);
 	$request->sign;
 	my $ua = LWP::UserAgent->new;
-	my $response = $ua->request(GET $url, Content => $request->to_post_body);
-	my $date = localtime();
-	if ( $response->is_success ) {
-		my $r = decode_json($response->content);
-		if($r->{'meta'}{'status'} == 201) {
-			print "[$date] Following tumblr entry queued\n";
-		} else {
-			printf("[$date] Cannot create Tumblr entry: %s\n",
-				$r->{'meta'}{'msg'});
-		}
-	} else {
-	printf("[$date] Cannot create Tumblr entry: %s\n",
-			$response->as_string);
-	}
-	return $response;
-}
+	my $authorization_signature = $request->to_authorization_header;
 
-# reblogging a random textpost from my personal blog as private (will reblog random textpost from dashboard later)
-sub reblogging {
-	my($comment) = $_[0]; # whatever comment i add to the post
-	my $request_params = %{ $_[1] };
+	my $message = GET  . $url . '?' . $request->normalized_message_parameters . 'Authorization' => $authorization_signature;
+	return $message;
 }
 
 my $blog = 'perlbot.tumblr.com';
--- trying.py
+++ trying.py
@@ -6,22 +6,22 @@ def generate_answer(question):
 	f = open('shitpost_database', 'r')
 	text = f.read().split("\n")
 	f.close()
-
+	
 	sentences = []
 	for i in range(10):
 		sentences.append(text[random.randint(0,len(text))-1])
 	for i in range(len(question)):
 		sentences.append(question[i])
 	print(sentences)
-
+	
 	wiki = []
 	for i in range(len(sentences)):
 		wiki.append(textblob.TextBlob(sentences[i]))
 		print(wiki[i].tags)
 	base_sentence = wiki[random.randint(0,10)]
-
-
-
+	
+	
+	
 	for i in range(len(sentences)):
 		text += sentences[i] + '\n'
 
