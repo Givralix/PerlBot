0ac740cae01e97f1c602be21ea8ef9c802e3edcb
Givralix <amelie.lulia68@gmail.com>
2016-08-12 11:33:52 +0200

    added Kindergarten Kid

--- trying.py
+++ trying.py
@@ -56,4 +56,4 @@ def generate_answer(question):
 		result += new_sentence[i] + " "
 	return result
 
-print(generate_answer(["If you could only save one, would you choose Garnet, Amethyst or Steven?"]))
+print(generate_answer(["maybe the real lesbian was the gays we made along the way"]))
