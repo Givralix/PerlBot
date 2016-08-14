9167e0013cf77532de91bf82b582bbbddfcecba3
Givralix <amelie.lulia68@gmail.com>
2016-08-14 12:47:40 +0200

    i guess

--- hourly_update_script.sh
+++ hourly_update_script.sh
@@ -1,11 +1,14 @@
+#!/bin/sh
 #########################
 # Hourly PerlBot Update #
 #########################
 
-rm -r /home/perlbot/PerlBot
-cd /home/perlbot/
-git clone https://github.com/Givralix/PerlBot.git
-perl /home/perlbot/PerlBot/perlbot.py >> /home/perlbot/PerlBot/log
-cd /home/perlbot/PerlBot
-git add chat_database log
-git commit -m "updated the log and maybe chat_database"
+date = ${date}
+log = $date" - log"
+rm -r /home/perlbot/PerlBot > /home/perlbot/log/$log
+cd /home/perlbot/ >> /home/perlbot/log/$log
+git clone https://github.com/Givralix/PerlBot.git >> /home/perlbot/log/$log
+perl /home/perlbot/PerlBot/perlbot.py >> /home/perlbot/log/$log
+cd /home/perlbot/PerlBot >> /home/perlbot/log/$log
+git add chat_database log >> /home/perlbot/log/$log
+git commit -m "updated the log and maybe chat_database" >> /home/perlbot/log/$log
