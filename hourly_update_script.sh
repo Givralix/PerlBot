50962229355202958f920b444115f50b5d2c5a28
bismuth <amelie.lulia68@gmail.com>
2016-09-11 09:11:07 +0200

    changed the commit message when perl receives an ask

--- hourly_update_script.sh
+++ hourly_update_script.sh
@@ -8,5 +8,5 @@ git pull
 perl perlbot.pl
 cd /home/bismuth/PerlBot
 git add chat_database answered_asks_ids
-git commit -m "updated the log and maybe chat_database and answered_asks_ids"
+git commit -m "updated chat_database and answered_asks_ids"
 git push
