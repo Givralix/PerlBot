46bb4d8b9bed12957f58e59f375fb6cbdc687bfc
Givralix <amelie.lulia68@gmail.com>
2017-03-15 18:20:23 +0100

    made a generate_sentence() function in perl! woo!

--- test.pl
+++ test.pl
@@ -30,15 +30,56 @@ my $blog = WWW::Tumblr::Blog->new(
 
 #my @submissions = @{$blog->posts_submission->{posts}};
 #print @submissions . "\n";
+my $markov = String::Markov->new(
+		order => 2,
+		sep => '',
+		split_sep => ' ',
+		join_sep => ' ',
+		null => "\0",
+		stable => 1,
+		normalize => undef,
+		do_chomp => 1,
+);
+my @list = (
+	"show_dialogue.txt",
+	"blog_dialogue.txt",
+);
+
+#open(DATA,"<show_dialogue.txt") or die "Can't open data";
+#my @lines = <DATA>;
+#close(DATA);
+
+my @files = (
+	"show_dialogue.txt",
+	"blog_dialogue.txt",
+);
+
+open $f, $files[0] or die "couldn't open 'show_dialogue.txt': $!";
+my @show_dialogue = <$f>;
+close FILE;
+
+open $f, $files[1] or die "couldn't open 'blog_dialogue.txt': $!";
+my @blog_dialogue = <$f>;
+close FILE;
+
+chomp(@show_dialogue);
+chomp(@blog_dialogue);
+
+my %forbidden_sentences;
+$forbidden_sentences{$_} = 1 for @show_dialogue;
+$forbidden_sentences{$_} = 1 for @blog_dialogue;
+
+$markov->add_files(@files);
+
 sub generate_sentence {
-	my $markov = String::Markov
-	with open("shitpost_database",'r',) as f:
-		text = f.read()
-	
-	with open("chat_database",'r',) as f:
-		text += f.read()
-	
-	text_model = markovify.NewlineText(text)
-	
-	return text_model.make_sentence(tries=100)
+	my $markov = $_[0];
+	my %forbidden_sentences = %{$_[1]};
+	my $sentence = $markov->generate_sample();
+	while ( $forbidden_sentences{$sentence} ) {
+		print $sentence . "\n";
+		$sentence = $markov->generate_sample();
+	}
+	return $sentence;
 }
+print $forbidden_sentences{"Hey, Steven..."} . "\n";
+print generate_sentence($markov, \%forbidden_sentences) . "\n";
